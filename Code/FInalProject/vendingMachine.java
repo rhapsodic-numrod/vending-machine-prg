0e8a4f9f5d7a592dd038b740f51e869a65721ca9
Marlven <mbhunushava@gmail.com>
2021-10-29 20:01:25 +0200

    Customer menu added

--- Code/FInalProject/vendingMachine.java
+++ Code/FInalProject/vendingMachine.java
@@ -155,150 +155,7 @@ public class vendingMachine {
             //#region Customer menu
             if (landingPageChoice.equals("*")){
                 custPrintAllItems(inventory, inventoryEnd);
-                System.out.print("How many items  do you want? ");
-            }
-            //#endregion Customer menu
-            //#region Owners menu
-            else if(landingPageChoice.equalsIgnoreCase(ownerPass)){
-                System.out.print("Enter verification password: ");
-                String verificationCode = userInput.next();
-                if (verificationCode.equalsIgnoreCase(verificationPass)){
-                    int ownersMenuChoice = -1;
-                    do{
-                        printOwnersMenu();
-                        ownersMenuChoice = userInput.nextInt();
-
-                        switch (ownersMenuChoice){
-                            case 1:{
-                                System.out.println("------ Restock/Add new items ------");
-                                System.out.println("Would you like to:");
-                                System.out.println("[1] Restock");
-                                System.out.println("[2] Add a new item");
-                                System.out.println("[3] Go back");
-                                int addRestockChoice = userInput.nextInt();
-                                switch (addRestockChoice){
-                                    case 1:{
-                                        System.out.println("------- Restock -------");
-                                        System.out.print("Enter item id: ");
-                                        String itemToBeRestocked = userInput.next();
-                                        // Find item
-                                        int itemRow = findItem(inventory, itemToBeRestocked);
-                                        // If item row is -1 than the item doesnt exist
-                                        if (itemRow == -1){
-                                            System.out.println("Item does not exist in inventory");
-                                            break;
-                                        } else{
-                                            // Print the items name
-                                            System.out.printf("Item name: %s Current Stock: %s%n",  inventory[itemRow][name], inventory[itemRow][stock]);
-                                            System.out.print("Enter amount to be added: ");
-                                            // Get ammount to be added
-                                            int amountToBeAdded = userInput.nextInt();
-                                            // Get the current stock from the item row
-                                            int currentStock = Integer.parseInt(inventory[itemRow][stock]);
-                                            // Add the new stock to the existing stock
-                                            int newStock = amountToBeAdded + currentStock;
-                                            // Change the value of the old stock to the new stock
-                                            inventory[itemRow][stock] = String.valueOf(newStock);
-                                            System.out.println("-----------------------------");
-                                            System.out.printf("Item name: %s Current Stock: %s%n",  inventory[itemRow][name], inventory[itemRow][stock]);
-                                            System.out.println("Process successfull :)");
-                                            break;
-                                        }
-                                    } case 2:{
-                                        System.out.println("---- Add a new item ----");
-                                        // Only add if the inventory is not full
-                                        if (inventoryEnd + 1 != rows){
-                                            // Get item information
-                                            System.out.print("Enter the item ID: ");
-                                            String newItemID = userInput.next();
-
-                                            System.out.print("Enter the item name: ");
-                                            String newItemName = userInput.next();
-
-<<<<<<< HEAD
-        // Customer menu
-        
-
-
-    public static int findItem(String[][] arr, String searchItem) {
-        int indexOfItem = 0, id = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i][id].equalsIgnoreCase(searchItem)) {
-                indexOfItem = i;
-                break;
-            } else {
-                indexOfItem = -1;
-            }
-        }
-        return indexOfItem;
-    }
-
-    public static void main(String[] args) {
-        int id = 0, name = 1, price = 2, stock = 3, end = -1, rows = 20, cols = 4;
-        String[][] inventory = new String[rows][cols];
-        /// Adding base items
-        String[] Item0 = {"C01", "Simba Salt and Vinegar Chips 132g", "15.00", "6"};
-        String[] Item1 = {"C02", "Simba Chilli Biltong Chips 132g", "15.00", "15"};
-        String[] Item2 = {"C03", "Simba Smoked Beef Chips 132g", "15.00", "2"};
-        String[] Item3 = {"C04", "Lay's Classic Chips 120g", "15.00", "24"};
-        String[] Item4 = {"C05", "Lay's Thai Sweet Chilli Chips 120g", "15.00", "56"};
-        String[] Item5 = {"C06", "Lay's Caribbean Onion & Balsamic Chips 120g", "15.00", "26"};
-        String[] Item6 = {"C07", "Doritos Sweet Chilli Pepper Corn Chips 150g", "15.00", "70"};
-        String[] Item7 = {"S01", "Nestle Aero Peppermint Chocolate Slab 85g", "10", "26"};
-        String[] Item8 = {"S02", "Snickers Duo Chocolate Bar 80g", "1.00", "2"};
-        String[] Item9 = {"S03", "Maynards Jelly Babies 125g", "2.50", "19"};
-        String[] Item10 = {"S04", "Nestle KitKat 11 Finger Dark Chocolate 135g", "10", "30"};
-        String[] Item11 = {"S05", "M&M's Peanut Chocolate 100g", "10", "26"};
-        String[] Item12 = {"D01", "Mineral Water 500ml", "11.00", "6"};
-        String[] Item13 = {"D02", "Natural Water 500ml", "11.00", "20"};
-        String[] Item14 = {"D03", "Powerade Island Burst 500ml", "12.00", "10"};
-        String[] Item15 = {"D04", "Coke 500ml", "12.00", "26"};
-        String[] Item16 = {"D05", "Fanta Grape 500ml", "12.00", "8"};
-        String[] Item17 = {"D06", "Fanta Orange 500ml", "12.00", "26"};
-        String[] Item18 = {"D07", "Liqui-Fruit Litchi Fruit Juice Box 250ml", "10", "6"};
-        String[] Item19 = {"D08", "Liqui-Fruit 100% Berry Blaze Fruit Juice Box 250ml", "10", "26"};
-
-        end++;
-        inventory[end] = Item0;
-        end++;
-        inventory[end] = Item1;
-        end++;
-        inventory[end] = Item2;
-        end++;
-        inventory[end] = Item3;
-        end++;
-        inventory[end] = Item4;
-        end++;
-        inventory[end] = Item5;
-        end++;
-        inventory[end] = Item6;
-        end++;
-        inventory[end] = Item7;
-        end++;
-        inventory[end] = Item8;
-        end++;
-        inventory[end] = Item9;
-        end++;
-        inventory[end] = Item10;
-        end++;
-        inventory[end] = Item11;
-        end++;
-        inventory[end] = Item12;
-        end++;
-        inventory[end] = Item13;
-        end++;
-        inventory[end] = Item14;
-        end++;
-        inventory[end] = Item15;
-        end++;
-        inventory[end] = Item16;
-        end++;
-        inventory[end] = Item17;
-        end++;
-        inventory[end] = Item18;
-        end++;
-        inventory[end] = Item19;
-
+               
         double cash[] = new double[11];
         cash[0] = 200;
         cash[1] = 100;
@@ -326,28 +183,30 @@ public class vendingMachine {
         numCash[10] = 200;
 
         int tHundreds = 0,hundreds = 0, fifties = 0, thirties = 0,twenties = 0, tens = 0, fives = 0, ones = 0, fiftyC = 0, tenC = 0, fiveC = 0;
-
-        Scanner rj = new Scanner(System.in);
-
+        int i = 0;
+        String[][] n = new String[rows][cols];
 
         double total = 0, priceItem = 0, priceOfItems = 0, change = 0, payment = 0;
         int QTY = 0, newStock = 0, itemRow = 0;
         System.out.println("How many different items do you want to buy?");
-        int a = rj.nextInt();
-        for (int i = 1; i <= a; i = i + 1) {
+        int a = userInput.nextInt();
+        for (i = 1; i <= a; i = i + 1) {
             System.out.println("Enter item code: ");
-            String x = rj.next();
+            String x = userInput.next();
+
             itemRow = findItem(inventory, x);
             // If item row is -1 than the item doesnt exist
             if (itemRow == -1) {
                 System.out.println("Item does not exist in inventory");
                 break;
-            } else if (itemRow != 1) {
+            } else if (itemRow != 1) {  //if item exists
+                // prints name of item chosen
                 System.out.println("Item selected: " + inventory[itemRow][name]);
-
+                n[a+1][1] = inventory[itemRow][name];
+                // asks for quantity of item to be input
                 System.out.println("Enter quantity of item: ");
-                QTY = rj.nextInt();
-
+                QTY = userInput.nextInt();
+                // prints quantity input
                 System.out.println("Quantity: " + QTY);
                 newStock = (Integer.parseInt(inventory[itemRow][stock])) - QTY;
 
@@ -361,8 +220,11 @@ public class vendingMachine {
                 System.out.println("Total due: N$" + total);
             }
         }
+
+
+
         System.out.println("Enter amount paid: ");
-        payment = rj.nextDouble();
+        payment = userInput.nextDouble();
 
         change = payment - total;
         System.out.println("Change: N$" + change);
@@ -474,17 +336,18 @@ public class vendingMachine {
                 changed = (changed % 10) * 2;
                 numCash[10] = numCash[10] - fiveC;
             }
-            System.out.println("Your change is disbursed as follows: N$200 X " + tHundreds + ", N$100 X " + hundreds + ", N$50 X" + fifties + ", N$30 X " + thirties + ", N$20 X " + twenties + ", N$10 X "  + tens + ", N$5 X " + fives + ", N$1 X " + ones + ", 50cents X " +fiftyC+ ", 10cents X "  + tenC + " and 5cents X " + fiveC);
+            System.out.println("Your change is disbursed as follows: N$200 X " + tHundreds + ", N$100 X " + hundreds + ", N$50 X" + fifties + ", N$30 X " + thirties + ", N$20 X " + twenties + ", N$10 X " + tens + ", N$5 X " + fives + ", N$1 X " + ones + ", 50cents X " + fiftyC + ", 10cents X " + tenC + " and 5cents X " + fiveC);
 
 
             System.out.println("Sammy's Sweets");
             System.out.println("417 Iverson Road");
             System.out.println("Windhoek");
             System.out.println("104-836-0217");
-            System.out.println("QTY "+ "     "+"Name "+"   "+"Amount");
+            System.out.println("QTY " + "     " + "Name " + "   " + "Amount");
             System.out.println("* * * * * * * * * * * * * *");
-            System.out.println(QTY+"    "+inventory[itemRow][name] + "     " + inventory[itemRow][price]);
-
+            for (i = 1; i <= a; i++) {
+                    System.out.println(QTY + "    " + n[a + 1][1] + "     " + priceItem);
+        }
             System.out.println("* * * * * * * * * * * * * *");
             System.out.println("Total"+"      "+total);
             System.out.println("Paid"+"      "+payment);
@@ -494,7 +357,65 @@ public class vendingMachine {
         else if(change < 0){
             System.out.println("We do not allow negatives. Please pay with a value greater than or equals to the total due");
         }
-=======
+    }
+            //#endregion Customer menu
+            //#region Owners menu
+            else if(landingPageChoice.equalsIgnoreCase(ownerPass)){
+                System.out.print("Enter verification password: ");
+                String verificationCode = userInput.next();
+                if (verificationCode.equalsIgnoreCase(verificationPass)){
+                    int ownersMenuChoice = -1;
+                    do{
+                        printOwnersMenu();
+                        ownersMenuChoice = userInput.nextInt();
+
+                        switch (ownersMenuChoice){
+                            case 1:{
+                                System.out.println("------ Restock/Add new items ------");
+                                System.out.println("Would you like to:");
+                                System.out.println("[1] Restock");
+                                System.out.println("[2] Add a new item");
+                                System.out.println("[3] Go back");
+                                int addRestockChoice = userInput.nextInt();
+                                switch (addRestockChoice){
+                                    case 1:{
+                                        System.out.println("------- Restock -------");
+                                        System.out.print("Enter item id: ");
+                                        String itemToBeRestocked = userInput.next();
+                                        // Find item
+                                        int itemRow = findItem(inventory, itemToBeRestocked);
+                                        // If item row is -1 than the item doesnt exist
+                                        if (itemRow == -1){
+                                            System.out.println("Item does not exist in inventory");
+                                            break;
+                                        } else{
+                                            // Print the items name
+                                            System.out.printf("Item name: %s Current Stock: %s%n",  inventory[itemRow][name], inventory[itemRow][stock]);
+                                            System.out.print("Enter amount to be added: ");
+                                            // Get ammount to be added
+                                            int amountToBeAdded = userInput.nextInt();
+                                            // Get the current stock from the item row
+                                            int currentStock = Integer.parseInt(inventory[itemRow][stock]);
+                                            // Add the new stock to the existing stock
+                                            int newStock = amountToBeAdded + currentStock;
+                                            // Change the value of the old stock to the new stock
+                                            inventory[itemRow][stock] = String.valueOf(newStock);
+                                            System.out.println("-----------------------------");
+                                            System.out.printf("Item name: %s Current Stock: %s%n",  inventory[itemRow][name], inventory[itemRow][stock]);
+                                            System.out.println("Process successfull :)");
+                                            break;
+                                        }
+                                    } case 2:{
+                                        System.out.println("---- Add a new item ----");
+                                        // Only add if the inventory is not full
+                                        if (inventoryEnd + 1 != rows){
+                                            // Get item information
+                                            System.out.print("Enter the item ID: ");
+                                            String newItemID = userInput.next();
+
+                                            System.out.print("Enter the item name: ");
+                                            String newItemName = userInput.next();
+
                                             System.out.print("Enter the item price: ");
                                             String newItemPrice = userInput.next();
                                             
@@ -542,7 +463,6 @@ public class vendingMachine {
                                             // Ask if user wants to continue
                                             System.out.println("[1] Go back\t[2] Continue");
                                             changePriceChoice = userInput.nextInt();
->>>>>>> 070ff552496e79bff8b93839c765ccb237ecd282
 
                                             if (changePriceChoice == 1){
                                                 System.out.println("O_O");
@@ -611,4 +531,3 @@ public class vendingMachine {
         } 
         userInput.close();
     }
-}
